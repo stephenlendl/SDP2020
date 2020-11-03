7832f548ae0e68d079957acb47c7cbf25970a135
ziidx <62824591+ziidx@users.noreply.github.com>
2020-11-02 23:15:23 -0500

    changes to app.js
    migrated page components into app.js so there is no need for external dependencies. added some UI design changes and templates for member/merchant sign-ins and account creation.

--- App.js
+++ App.js
@@ -17,23 +17,127 @@ import {
   Button,
   TextInput
 } from 'react-native';
-import Home from './routeComponents/Home';
-import page1 from './routeComponents/page1';
-import page2 from './routeComponents/page2';
-import page3 from './routeComponents/page3';
-import Error from './routeComponents/Error';
 import { NativeRouter, Route, Switch, Link } from 'react-router-native';
+
+const welcomePage = ({history}) => {
+  return (
+  <View>
+    <Text style={styles.header}>
+      Welcome to NoID Test Home Page!{"\n"}
+      Select an Account to Login
+    </Text>
+    <View style={styles.container}>
+      <Button title="Member Login" onPress = {() => history.push("/memberLogin")} />
+      <Button title="Merchant Login" onPress = {() => history.push("/merchantLogin")} />
+      <Button title="Create an Account" onPress = {() => history.push("/accountCreation")} />
+    </View>
+  </View>
+  );
+}
+
+const memLogin = ({history}) => {
+  const [value, onChangeText] = React.useState('');
+  return (
+    <View>
+        <Text style={styles.header}>Welcome to NoID Member Login!</Text>
+        <View style={styles.container}>
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: "white", width: 300}}
+            onChangeText={text => onChangeText(text)}
+            placeholder={'Username'}
+            value={value}
+          />
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: "white", width: 300}}
+            onChangeText={text => onChangeText(text)}
+            placeholder={'Password'}
+            value={value}
+          />
+          <Button title="Go to Home Page" onPress = {() => history.push("/")} />
+          <Button title="ERROR" onPress = {() => history.push("/error")} />
+        </View>
+        
+    </View>
+  );
+}
+
+const merchLogin = ({history}) => {
+  const [value, onChangeText] = React.useState('');
+  return (
+    <View>
+        <Text style={styles.header}>Welcome to NoID Merchant Login!</Text>
+        <View style={styles.container}>
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: "white", width: 300}}
+            onChangeText={text => onChangeText(text)}
+            placeholder={'Username'}
+            value={value}
+          />
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: "white", width: 300}}
+            onChangeText={text => onChangeText(text)}
+            placeholder={'Password'}
+            value={value}
+          />
+          <Button title="Go to Home Page" onPress = {() => history.push("/")} />
+          <Button title="ERROR" onPress = {() => history.push("/error")} />
+        </View>
+    </View>
+  );
+}
+
+const accMake = ({history}) => {
+  const [value, onChangeText] = React.useState('');
+  return( 
+    <View>
+        <Text style={styles.header}>Welcome to NoID Account Creation!</Text>
+        <View style={styles.container}>
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: "white", width: 300}}
+            onChangeText={text => onChangeText(text)}
+            placeholder={'Create Username'}
+            value={value}
+          />
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: "white", width: 300}}
+            onChangeText={text => onChangeText(text)}
+            placeholder={'Create Password'}
+            value={value}
+          />
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: "white", width: 300}}
+            onChangeText={text => onChangeText(text)}
+            placeholder={'Email Address'}
+            value={value}
+          />
+          <Button title="Go to Home Page" onPress = {() => history.push("/")} />
+          <Button title="ERROR" onPress = {() => history.push("/error")} />
+        </View>
+    </View>
+  )
+}
+
+const errorPage = ({history}) => {
+  return(
+    <View>
+        <Text style={styles.header}> There was an Error! :( </Text>
+        <View style={styles.container}>
+          <Button title="Back to Home" onPress= {()=> history.push("/")} />
+        </View>
+    </View>
+  );
+}
 
 const App = () => {
   return (
   <NativeRouter>
     <View style={styles.container}>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/page1" component={page1} />
-        <Route path="/page2" component={page2} />
-        <Route path="/page3" component={page3} />
-        <Route path="/ERROR" component={Error} />
+        <Route exact path="/" component={welcomePage} />
+        <Route path="/memberLogin" component={memLogin} />
+        <Route path="/merchantLogin" component={merchLogin} />
+        <Route path="/accountCreation" component={accMake} />
+        <Route path="/error" component={errorPage} />
       </Switch>
     </View>
   </NativeRouter>
@@ -44,10 +148,12 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        backgroundColor: "green"
     },
     header: {
-        fontSize: 20
+        fontSize: 20,
+        marginTop: 20
     },
     nav: {
         flexDirection: "row",
